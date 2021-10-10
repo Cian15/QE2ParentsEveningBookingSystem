a322436f188cc9eb9c653adc343267404a9e7213
CianDickinson <ciandickinson15@gmail.com>
2021-10-10 22:49:29 +0100

    Add project files.

---
+++ QE2ParentsEveningBookingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace QE2ParentsEveningBookingSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
